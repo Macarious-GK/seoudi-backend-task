6f482cb3c49deac54b07da4045148a2621199d9a
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2023-08-07 12:34:25 +0300

    brute force installation

--- build.sh
+++ build.sh
@@ -2,8 +2,14 @@
 # exit on error
 set -o errexit
 
-pip install Pipfile
 pip install django
+pip install pipfile
+pip install djangorestframework
+pip install django-debug-toolbar
+pip install djangorestframework-xml
+pip install bleach
+pip install django-filter
+pip install djoser
 
 python manage.py makemigrations
 python manage.py migrate
